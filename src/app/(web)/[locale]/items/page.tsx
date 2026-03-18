e5ee83fd813db05f1611b500d630f146ad5fed49
Volodymyr <volus1986@gmail.com>
2026-03-18 19:09:36 +0200

    feat: enhance items layout with data fetching and hydration using react-query

--- src/app/(web)/[locale]/items/page.tsx
+++ src/app/(web)/[locale]/items/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { usePostsQuery } from "@/features/get-posts";
+import { Button } from "@/shared/ui";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import { useTranslations } from "next-intl";
 
 export default function Items() {
   const t = useTranslations("ItemsPage");
 
-  return <main>{t("title")}</main>;
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = Number(searchParams.get("limit")) || 10;
+
+  const posts = usePostsQuery(page, limit);
+
+  const postsEls = posts.data?.map((post) => {
+    return (
+      <div key={post.id} className="flex gap-8">
+        <div>{post.id}</div>
+        <div>{post.userId}</div>
+        <div>{post.title}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {postsEls}
+    </div>
+  );
 }
--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,24 +1,35 @@
-import type { Metadata } from "next";
 import "@/app/globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import Providers from "../providers";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { postsApi } from "@/entities/posts";
 import { Header } from "@/widgets";
 
-export const metadata: Metadata = {
-  title: "rl hw 260312",
-  description: "rl hw 260312",
-};
-
-export default function RootLayout({
+export default async function ItemsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+  const page = 1;
+  const limit = 20;
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", page, limit],
+    queryFn: () => postsApi.getPosts(page, limit),
+  });
+
   return (
     <NextIntlClientProvider>
       <Providers>
-        <Header />
-        {children}
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <Header />
+          {children}
+        </HydrationBoundary>
       </Providers>
     </NextIntlClientProvider>
   );
