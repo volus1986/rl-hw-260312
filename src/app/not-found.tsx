f44ebad168910392aa14564fd01776fb3ef88240
Volodymyr <volus1986@gmail.com>
2026-03-19 15:00:53 +0200

    refactor: replace ErrorPage component with a new NotFound component, enhancing layout and functionality

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,35 @@
-import Error from "@/shared/ui/components/shadcn-studio/blocks/error-page-01/error-page-01";
+import { Button } from "@/shared/ui";
+import Link from "next/link";
 
-const ErrorPage = () => {
-  return <Error />;
+const Error = () => {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div
+        className="flex flex-col items-center justify-center px-4 py-8
+          text-center">
+        <h2 className="mb-6 text-5xl font-semibold">Whoops!</h2>
+        <h3 className="mb-1.5 text-3xl font-semibold">Something went wrong</h3>
+        <p className="text-muted-foreground mb-6 max-w-sm">
+          The page you&apos;re looking for isn&apos;t found, we suggest you back
+          to home.
+        </p>
+        <Button asChild size="lg" className="rounded-lg text-base">
+          <Link href="/">Back to home page</Link>
+        </Button>
+      </div>
+
+      {/* Right Section: Illustration */}
+      <div className="relative max-h-screen w-full p-2 max-lg:hidden">
+        <div className="h-full w-full rounded-2xl bg-black"></div>
+        <img
+          src="https://cdn.shadcnstudio.com/ss-assets/blocks/marketing/error/image-1.png"
+          alt="404 illustration"
+          className="absolute top-1/2 left-1/2 h-[clamp(260px,25vw,406px)]
+            -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+    </div>
+  );
 };
 
-export default ErrorPage;
+export default Error;
