0aa8bf13310eea4eef1054cf9dc1613a76b4f2ea
Volodymyr <volus1986@gmail.com>
2026-03-19 12:31:26 +0200

    refactor: optimize getPost function in postsApi with caching

--- src/entities/posts/api/index.ts
+++ src/entities/posts/api/index.ts
@@ -1,7 +1,8 @@
+import { cache } from "react";
 import getPost from "./get-post";
 import getPosts from "./get-posts";
 
 export const postsApi = {
   getPosts,
-  getPost,
+  getPost: cache((id: number) => getPost(id)),
 };
