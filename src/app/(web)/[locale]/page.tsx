c2f565b9879edad7ce2f74d9d7b0b1514c629d7e
Volodymyr <volus1986@gmail.com>
2026-03-20 13:26:20 +0200

    refactor: restructure RootPage to use MainComponent for improved modularity and maintainability

--- src/app/(web)/[locale]/page.tsx
+++ src/app/(web)/[locale]/page.tsx
@@ -1,17 +1,7 @@
-import { Link } from "@/i18n/navigation";
-import { useTranslations } from "next-intl";
+import { MainComponent } from "@/app/modules/main";
 
-export default function RootPage() {
-  const t = useTranslations("RootPage");
+const MainPage = () => {
+  return <MainComponent />;
+};
 
-  return (
-    <div className="flex flex-col items-center justify-center mt-80">
-      <div>
-        <Link href={"items"}>{t("items")}</Link>
-      </div>
-      <div>
-        <Link href={"login"}>{t("login")}</Link>
-      </div>
-    </div>
-  );
-}
+export default MainPage;
---
+++ src/app/modules/main/main.component.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+
+export const MainComponent = () => {
+  const t = useTranslations("RootPage");
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-80">
+      <div>
+        <Link href={"items"}>{t("items")}</Link>
+      </div>
+      <div>
+        <Link href={"login"}>{t("login")}</Link>
+      </div>
+    </div>
+  );
+};
