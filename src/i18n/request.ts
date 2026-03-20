4ef11bf7c586c81f8828f22157e0404bf1269f7d
Volodymyr <volus1986@gmail.com>
2026-03-20 09:16:34 +0200

    refactor: localization files structure

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,18 +1,18 @@
-import {getRequestConfig} from 'next-intl/server';
-import {hasLocale} from 'next-intl';
-import {routing} from './routing';
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+import { routing } from "./routing";
 
-export default getRequestConfig(async ({requestLocale}) => {
+export default getRequestConfig(async ({ requestLocale }) => {
   // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
 
-  const messages = (await import(`../../messages/${locale}.json`)).default;
+  const messages = (await import(`../../translations/${locale}.json`)).default;
 
   return {
     locale,
-    messages
+    messages,
   };
 });
