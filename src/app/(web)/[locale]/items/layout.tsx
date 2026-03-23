827c5bc75481defdd632063a7d20b3eed988467e
Volodymyr <volus1986@gmail.com>
2026-03-23 18:14:38 +0200

    fix: Items page preload (prefetching)

--- src/app/(web)/[locale]/items/layout.tsx
+++ src/app/(web)/[locale]/items/layout.tsx
@@ -1,30 +1,9 @@
 import Providers from "@/app/providers";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import { postsApi } from "@/app/entities/api";
 
 export default async function ItemsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient();
-  const page = 1;
-  const limit = 20;
-
-  await queryClient.prefetchQuery({
-    queryKey: ["posts", page, limit],
-    queryFn: () => postsApi.getPosts(page, limit),
-  });
-
-  return (
-    <Providers>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        {children}
-      </HydrationBoundary>
-    </Providers>
-  );
+  return <Providers>{children}</Providers>;
 }
--- src/app/(web)/[locale]/items/page.tsx
+++ src/app/(web)/[locale]/items/page.tsx
@@ -1,7 +1,34 @@
-import { ItemsModule } from "@/app/modules/items";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const ItemsPage = () => {
-  return <ItemsModule />;
+import { ItemsModule } from "@/app/modules/items";
+import { postsApi } from "@/app/entities/api";
+
+const ItemsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const queryClient = new QueryClient();
+  const queries = await searchParams;
+
+  console.log(queries);
+  const page = Number(queries.page) || 1;
+  const limit = 20;
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", page, limit],
+    queryFn: () => postsApi.getPosts(page, limit),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ItemsModule />
+    </HydrationBoundary>
+  );
 };
 
 export default ItemsPage;
