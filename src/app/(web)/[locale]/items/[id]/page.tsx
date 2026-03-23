894209c1eb8b3ba016221bb4164102b9fb710417
Volodymyr <volus1986@gmail.com>
2026-03-23 18:14:53 +0200

    refactor: remove unused searchParams from items page props

--- src/app/(web)/[locale]/items/[id]/page.tsx
+++ src/app/(web)/[locale]/items/[id]/page.tsx
@@ -6,7 +6,6 @@ import { ItemModule } from "@/app/modules/item";
 
 type Props = {
   params: Promise<{ id: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
