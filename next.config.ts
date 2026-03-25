1ef0ff016370b6156b1b63b07cc7f5d26bd56f39
Volodymyr <volus1986@gmail.com>
2026-03-25 18:13:30 +0200

    refactor: enhance logging configuration in next.config.ts for better fetch visibility in non-production environments

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
   // eslint: { ignoreDuringBuilds: true }
+
+  logging: {
+    fetches: {
+      fullUrl: process.env.NODE_ENV !== 'production',
+    },
+  },
 };
 
 // export default nextConfig;
 
-const withNextIntl = createNextIntlPlugin("./src/pkg/locale/request.ts");
+const withNextIntl = createNextIntlPlugin('./src/pkg/locale/request.ts');
 export default withNextIntl(nextConfig);
