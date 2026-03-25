f058e4be388e80f19f2f07c3847b00d78938e650
Volodymyr <volus1986@gmail.com>
2026-03-25 11:46:56 +0200

    fix: tests & configs

--- tests/e2e/items-page.spec.ts
+++ tests/e2e/items-page.spec.ts
@@ -1,37 +1,47 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("has title", async ({ page }) => {
-  await page.goto("/items");
+test('items page', async ({ page }) => {
+  await page.goto('/items');
 
   // await page.pause();
 
-  await expect(page.getByRole("heading", { name: "Items" })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Items' })).toBeVisible();
 
   await expect(
-    page.getByRole("columnheader", { name: "ID", exact: true }),
+    page.getByRole('columnheader', { name: 'ID', exact: true }),
   ).toBeVisible();
 
   await expect(
-    page.getByRole("columnheader", { name: "User ID" }),
+    page.getByRole('columnheader', { name: 'User ID' }),
   ).toBeVisible();
 
-  await expect(page.getByRole("columnheader", { name: "Title" })).toBeVisible();
+  await expect(page.getByRole('columnheader', { name: 'Title' })).toBeVisible();
 
   await expect(
-    page.getByRole("button", { name: "Prev", exact: true }),
+    page.getByRole('button', { name: 'Prev', exact: true }),
   ).toBeDisabled();
 
   await expect(
-    page.getByRole("button", { name: "Next", exact: true }),
+    page.getByRole('button', { name: 'Next', exact: true }),
   ).toBeVisible();
 
-  await page.getByRole("button", { name: "Next", exact: true }).click();
+  await page.getByRole('button', { name: 'Next', exact: true }).click();
 
   await expect(
-    page.getByRole("button", { name: "Next", exact: true }),
+    page.getByRole('button', { name: 'Next', exact: true }),
   ).toBeEnabled();
 
   await expect(
-    page.getByRole("button", { name: "Prev", exact: true }),
+    page.getByRole('button', { name: 'Prev', exact: true }),
+  ).toBeEnabled();
+
+  await page.getByRole('button', { name: 'Prev', exact: true }).click();
+
+  await expect(
+    page.getByRole('button', { name: 'Next', exact: true }),
+  ).toBeEnabled();
+
+  await expect(
+    page.getByRole('button', { name: 'Prev', exact: true }),
   ).toBeDisabled();
 });
