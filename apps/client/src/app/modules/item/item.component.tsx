196af59f81bb543725281ceac2b84786d152cfb4
Volodymyr <volus1986@gmail.com>
2026-03-26 20:56:35 +0200

    refactor: replace table structure with reusable row component for improved readability in item display

--- apps/client/src/app/modules/item/item.component.tsx
+++ apps/client/src/app/modules/item/item.component.tsx
@@ -3,7 +3,7 @@
 import { useTranslations } from 'use-intl';
 
 import { type PostDetails } from '@/app/entities/models';
-import { Button } from '@/app/shared/ui';
+import { Button, Table, TableBody, TableCell, TableRow } from '@/app/shared/ui';
 import { useRouter } from '@/pkg/locale';
 
 const ItemComponent = ({ item }: { item: PostDetails }) => {
@@ -14,34 +14,32 @@ const ItemComponent = ({ item }: { item: PostDetails }) => {
     router.back();
   };
 
+  const rowEl = (title: string, value: number | string) => {
+    return (
+      <TableRow className='font-medium'>
+        <TableCell>{title}</TableCell>
+        <TableCell className='text-wrap'>{value}</TableCell>
+      </TableRow>
+    );
+  };
+
   return (
-    <div>
-      <h1 className='text-center'>{t('title')}</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td>{t('postId')}</td>
-            <td>{item.id}</td>
-          </tr>
+    <div className='grid justify-center'>
+      <div className='text-wrap'>
+        <h1 className='text-center'>{t('title')}</h1>
+        <Table>
+          <TableBody>
+            {rowEl(t('postId'), item.id)}
+            {rowEl(t('postUserId'), item.userId)}
+            {rowEl(t('postTitle'), item.title)}
+            {rowEl(t('postDescription'), item.body)}
+          </TableBody>
+        </Table>
 
-          <tr>
-            <td>{t('postUserId')}</td>
-            <td>{item.userId}</td>
-          </tr>
-
-          <tr>
-            <td>{t('postTitle')}</td>
-            <td>{item.title}</td>
-          </tr>
-
-          <tr>
-            <td className='pr-4'>{t('postDescription')}</td>
-            <td>{item.body}</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <Button onClick={handlePreviousPageButtonClick}>{t('previousPageButton')}</Button>
+        <Button className='mt-8' onClick={handlePreviousPageButtonClick}>
+          {t('previousPageButton')}
+        </Button>
+      </div>
     </div>
   );
 };
